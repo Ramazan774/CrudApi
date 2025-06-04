2df24978930c77f851d3a394e206bf777dc641c0
Ramazan774 <ramazan.ramazanov@glion.ch>
2025-06-03 20:45:46 -0500

    added Get

--- Program.cs
+++ Program.cs
@@ -4,11 +4,23 @@ var app = builder.Build();
 var blogs = new List<Blog>
 {
   new Blog { Title = "My First Post", Body = "This is my first post"},
-  new Blog { Title = "My Second Post", Body = "This is my second post"},
-}
+  new Blog { Title = "My Second Post", Body = "This is my second post"}
+};
 
 app.MapGet("/", () => "I am root!");
 
+app.MapGet("/blogs", () => {
+  return blogs;
+});
+
+app.MapGet("/blogs/{id}", (int id) =>{
+  if (id < 0 || id >= blogs.Count) {
+    return Results.NotFound();
+  } else {
+    return Results.Ok(blogs[id]);
+  }
+  
+});
 app.Run();
 
 public class Blog
